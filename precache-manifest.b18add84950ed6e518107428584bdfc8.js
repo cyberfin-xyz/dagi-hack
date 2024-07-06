154036c97ea61220a26f8024f8a0038860f2cdd8
Mischuk Roman <50909715+MischukRoman@users.noreply.github.com>
2024-07-06 04:19:50 +0300

    Updates

---
+++ precache-manifest.b18add84950ed6e518107428584bdfc8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed32b33cab92a34bb32f7253610eb155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f21a8b4345f81bab4c01",
+    "url": "/static/css/main.fedb261a.chunk.css"
+  },
+  {
+    "revision": "831b2f8d5e4e3637347e",
+    "url": "/static/js/2.d5d77e35.chunk.js"
+  },
+  {
+    "revision": "f21a8b4345f81bab4c01",
+    "url": "/static/js/main.e3dd1524.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1559ffc7cf61cbae7ea55a250722009c",
+    "url": "/static/media/WorkSans-Bold.1559ffc7.ttf"
+  },
+  {
+    "revision": "73f5c36f3da9740c41b2c1aeb7fc94e5",
+    "url": "/static/media/WorkSans-Medium.73f5c36f.ttf"
+  },
+  {
+    "revision": "a3d6c7f7606fc33a6ab5bed9688d1fe8",
+    "url": "/static/media/WorkSans-Regular.a3d6c7f7.ttf"
+  },
+  {
+    "revision": "c1b2dde1705db3ada7fd668c36523f66",
+    "url": "/static/media/WorkSans-SemiBold.c1b2dde1.ttf"
+  },
+  {
+    "revision": "07f66a816d3d2a36d4ccb3f7c0b0308f",
+    "url": "/static/media/add-img-icon.07f66a81.svg"
+  },
+  {
+    "revision": "17363a0b350b164dd56c05e352556527",
+    "url": "/static/media/bobby-token.17363a0b.png"
+  },
+  {
+    "revision": "2ffef1e82a03fde7d657526f0b57aca1",
+    "url": "/static/media/loader.2ffef1e8.svg"
+  },
+  {
+    "revision": "6f30807eab05c85239e6dc62f34bd8bf",
+    "url": "/static/media/plus-icon.6f30807e.svg"
+  },
+  {
+    "revision": "65f1690f8aed12bd00e5d23264700ad2",
+    "url": "/static/media/search-icon.65f1690f.svg"
+  },
+  {
+    "revision": "acd9829327fdd829fe7af2f44bd71f5a",
+    "url": "/static/media/stars-icon.acd98293.svg"
+  }
+]);
