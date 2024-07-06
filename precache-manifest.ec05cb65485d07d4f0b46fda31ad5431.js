e7e33a19fd7aa26e3ff6be0c4c8c19660f820ec5
Mischuk Roman <50909715+MischukRoman@users.noreply.github.com>
2024-07-06 04:21:47 +0300

    Updates

---
+++ precache-manifest.ec05cb65485d07d4f0b46fda31ad5431.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cba233abb779b25c0ab682bf8a01661",
+    "url": "/dagi-hack/index.html"
+  },
+  {
+    "revision": "c17e52c04933d85f1f56",
+    "url": "/dagi-hack/static/css/main.1d2f8ebe.chunk.css"
+  },
+  {
+    "revision": "599d5fedda4ce1dacf9d",
+    "url": "/dagi-hack/static/js/2.c0574cb9.chunk.js"
+  },
+  {
+    "revision": "c17e52c04933d85f1f56",
+    "url": "/dagi-hack/static/js/main.e3dd1524.chunk.js"
+  },
+  {
+    "revision": "0a7cebbe6de5c1ec4bda",
+    "url": "/dagi-hack/static/js/runtime~main.8720d6ed.js"
+  },
+  {
+    "revision": "1559ffc7cf61cbae7ea55a250722009c",
+    "url": "/dagi-hack/static/media/WorkSans-Bold.1559ffc7.ttf"
+  },
+  {
+    "revision": "73f5c36f3da9740c41b2c1aeb7fc94e5",
+    "url": "/dagi-hack/static/media/WorkSans-Medium.73f5c36f.ttf"
+  },
+  {
+    "revision": "a3d6c7f7606fc33a6ab5bed9688d1fe8",
+    "url": "/dagi-hack/static/media/WorkSans-Regular.a3d6c7f7.ttf"
+  },
+  {
+    "revision": "c1b2dde1705db3ada7fd668c36523f66",
+    "url": "/dagi-hack/static/media/WorkSans-SemiBold.c1b2dde1.ttf"
+  },
+  {
+    "revision": "07f66a816d3d2a36d4ccb3f7c0b0308f",
+    "url": "/dagi-hack/static/media/add-img-icon.07f66a81.svg"
+  },
+  {
+    "revision": "17363a0b350b164dd56c05e352556527",
+    "url": "/dagi-hack/static/media/bobby-token.17363a0b.png"
+  },
+  {
+    "revision": "2ffef1e82a03fde7d657526f0b57aca1",
+    "url": "/dagi-hack/static/media/loader.2ffef1e8.svg"
+  },
+  {
+    "revision": "6f30807eab05c85239e6dc62f34bd8bf",
+    "url": "/dagi-hack/static/media/plus-icon.6f30807e.svg"
+  },
+  {
+    "revision": "65f1690f8aed12bd00e5d23264700ad2",
+    "url": "/dagi-hack/static/media/search-icon.65f1690f.svg"
+  },
+  {
+    "revision": "acd9829327fdd829fe7af2f44bd71f5a",
+    "url": "/dagi-hack/static/media/stars-icon.acd98293.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b18add84950ed6e518107428584bdfc8.js"
+  "/dagi-hack/precache-manifest.ec05cb65485d07d4f0b46fda31ad5431.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dagi-hack/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
